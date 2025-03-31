647d892b68042f25399f3ae21b4d4df224787b8d
lammmmmmmmmmmm <vuthanhlam24702@gmail.com>
2025-03-31 17:35:44 +0700

    feat: turn off item collision while carrying

--- Assets/_Project/Item/Scripts/ItemCarrier.cs
+++ Assets/_Project/Item/Scripts/ItemCarrier.cs
@@ -1,18 +1,28 @@
+using System;
 using UnityEngine;
 
 namespace Item {
     public class ItemCarrier : MonoBehaviour {
         [SerializeField] private LayerMask itemLayer;
         public bool IsCarrying { get; private set;}
+        
+        private Rigidbody2D _rb;
+
+        private void Awake() {
+            _rb = GetComponent<Rigidbody2D>();
+        }
 
         private void OnCollisionEnter2D(Collision2D other) {
             if (!IsCarrying && itemLayer.Contains(other.gameObject.layer)) {
                 IsCarrying = true;
+                
+                _rb.excludeLayers = _rb.excludeLayers.AddLayerMasks(itemLayer);
                 Destroy(other.gameObject);
             }
         }
 
         public void ResetCarrying() {
+            _rb.excludeLayers = _rb.excludeLayers.RemoveLayerMasks(itemLayer);
             IsCarrying = false;
         }
     }
--- Assets/_Project/Item/Scripts/LayerMaskExtension.cs
+++ Assets/_Project/Item/Scripts/LayerMaskExtension.cs
@@ -5,5 +5,13 @@ namespace Item {
         public static bool Contains(this LayerMask mask, int layer) {
             return mask == (mask | (1 << layer));
         }
+
+        public static LayerMask AddLayerMasks(this LayerMask mask, LayerMask maskB) {
+            return mask | maskB; // Bitwise OR combines both LayerMasks
+        }
+
+        public static LayerMask RemoveLayerMasks(this LayerMask mask, LayerMask maskB) {
+            return mask ^ maskB;
+        }
     }
 }
