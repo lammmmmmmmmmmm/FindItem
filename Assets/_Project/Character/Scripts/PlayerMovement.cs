c514c2af1a6510df94ae81b109e5aef41691850d
lammmmmmmmmmmm <vuthanhlam24702@gmail.com>
2025-04-04 20:26:08 +0700

    fix: joystick

--- Assets/_Project/Character/Scripts/PlayerMovement.cs
+++ Assets/_Project/Character/Scripts/PlayerMovement.cs
@@ -3,30 +3,31 @@ using UnityEngine.InputSystem;
 
 namespace Character {
     public class PlayerMovement : MonoBehaviour {
+        [SerializeField] private Joystick joystick;
         [SerializeField] private float defaultMoveSpeed = 10f;
 
         public float DefaultMoveSpeed => defaultMoveSpeed;
         public float CurrentMoveSpeed { get; set; }
 
         private Rigidbody2D _rb;
-        private PlayerInput _playerInput;
-        private InputAction _moveAction;
         private FogOfWall _fogOfWall;
 
         private void Awake() {
             _rb = GetComponent<Rigidbody2D>();
-            _playerInput = GetComponent<PlayerInput>();
-            _moveAction = _playerInput.actions["Move"];
+            _fogOfWall = GetComponentInChildren<FogOfWall>();
 
             CurrentMoveSpeed = defaultMoveSpeed;
-            _fogOfWall = GetComponentInChildren<FogOfWall>();
         }
 
         private void FixedUpdate() {
-            Vector2 moveDirection = _moveAction.ReadValue<Vector2>();
-            _fogOfWall?.UpdateRotation(moveDirection);
-            moveDirection *= CurrentMoveSpeed;
-            _rb.velocity = moveDirection;
+            if (joystick.Direction.y != 0 || joystick.Direction.x != 0) {
+                Vector2 moveDirection = new Vector2(joystick.Direction.x, joystick.Direction.y);
+                _rb.velocity = moveDirection * 10f; // Adjust speed as needed
+            
+                _fogOfWall?.UpdateRotation(moveDirection);
+            } else {
+                _rb.velocity = Vector2.zero; // Stop movement when joystick is not used
+            }
         }
         
         public void SetMoveSpeed(float moveSpeed) {
